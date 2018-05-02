1ffc2ef36bf1cb977142247291c7be95f2f4b267
cxlwill <cxlwill@outlook.com>
2018-05-02 21:19:46 +0800

    小修改

--- package/opt/hassbian/suites/x86.sh
+++ package/opt/hassbian/suites/x86.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 function x86-show-short-info {
-  echo "x86环境配置"
+  echo "x86环境配置（使用root用户）"
 }
 
 function x86-show-long-info {
-  echo "x86环境配置"
+  echo "x86环境配置（使用root用户）"
 }
 
 function x86-show-copyright-info {
@@ -17,10 +17,8 @@ x86-show-short-info
 x86-show-copyright-info
 
 echo "创建homeassistant用户及用户组"
-on_chroot << EOF
 groupadd -f -r -g 1001 homeassistant
 useradd -u 1001 -g 1001 -rm homeassistant
-EOF
 
 echo "创建homeassistant安装环境"
 install -v -o 1001 -g 1001 -d /srv/homeassistant
