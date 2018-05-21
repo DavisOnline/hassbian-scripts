863610e7ef9fcc0d9e36e20b915ff81139644dd0
cxlwill <cxlwill@outlook.com>
2018-05-21 21:03:28 +0800

    typo

--- package/opt/hassbian/suites/nodered.sh
+++ package/opt/hassbian/suites/nodered.sh
@@ -39,6 +39,7 @@ cd ~/.node-red
 npm install node-red-contrib-home-assistant
 
 echo "设置自动启动"
+cd /home/pi
 sudo wget https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/nodered.service -O /lib/systemd/system/nodered.service
 sudo wget https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/node-red-start -O /usr/bin/node-red-start
 sudo wget https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/node-red-stop -O /usr/bin/node-red-stop
